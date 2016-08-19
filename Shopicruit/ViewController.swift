d227067088c3397b05295310058746e3b2d17335
Fabian Vergara <vergarf@gmail.com>
2016-08-18 19:48:54 -0500

    Initial Commit

---
+++ Shopicruit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shopicruit
+//
+//  Created by Fabian Vergara on 2016-08-18.
+//  Copyright © 2016 fvergara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
